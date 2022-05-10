f391cea2f70895303de45090a9482885d697bc62
BreX900 <brexmaster@gmail.com>
2022-05-10 09:02:36 +0200

    feat wip cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,49 @@
 import * as functions from "firebase-functions";
+import {firestore} from "./lib/instances";
+import { MatchDto } from "./lib/matches/MatchDto";
+import { StatDto } from "./lib/stats/StatDto";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Le statistiche per giocatore
+// Le statistiche per squadra
+
+functions.firestore
+  .document("matches/{docId}")
+  .onCreate(async (snapshot, context) => {
+    const match = snapshot.data() as MatchDto;
+
+    const statSnapshot = await firestore
+      .collection("stats")
+      .where("players", "array-contains", match.leftPlayerIds)
+      .get();
+
+    let stat: StatDto;
+    if (statSnapshot.docs.length == 0) {
+      stat = {
+        // Generate document id
+        id: firestore.collection("stats").doc().id,
+        players: match.leftPlayerIds,
+        matchesLost: 0,
+        matchesWins: 0,
+        goalsConceded: 0,
+        goalsScored: 0,
+      };
+    } else {
+      const doc = statSnapshot.docs[0];
+      stat = {
+        ...(doc.data() as StatDto),
+        id: doc.id,
+      };
+    }
+
+    const isLeftWinner = match.leftPoints > match.rightPoint;
+
+    let updatedStat = {
+      ...stat,
+      matchesWins: isLeftWinner ? stat.matchesWins + 1 : stat.matchesWins,
+      matchesLost: isLeftWinner ? stat.matchesLost : stat.matchesLost + 1,
+      goalsScored: stat.goalsScored + match.leftPoints,
+      goalsConceded: stat.goalsConceded + match.rightPoint,
+    };
+
+    await firestore.doc(stat.id).set(updatedStat);
+  });
