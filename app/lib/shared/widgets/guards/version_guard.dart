925b95519d3c0ffdb7f805e2849124a26c57dfda
BreX900 <brexmaster@gmail.com>
2022-10-09 17:14:46 +0200

    fix: min version

--- app/lib/shared/widgets/guards/version_guard.dart
+++ app/lib/shared/widgets/guards/version_guard.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:mek_gasol/modules/doof/shared/service_locator/service_locator.dart';
 import 'package:mek_gasol/shared/theme.dart';
@@ -20,14 +21,15 @@ class VersionGuard extends StatefulWidget {
 class _VersionGuardState extends State<VersionGuard> {
   bool _isBlocked = false;
   Version? _currentVersion;
-  Version? _targetVersion;
+  Version? _minVersion;
 
   @override
   void initState() {
     super.initState();
-    _checkVersion();
+    if (kReleaseMode) _checkVersion();
   }
 
+  // "version" field in the document is deprecated
   void _checkVersion() async {
     final packageInfo = await PackageInfo.fromPlatform();
     _currentVersion = Version.parse(packageInfo.version);
@@ -38,13 +40,13 @@ class _VersionGuardState extends State<VersionGuard> {
   }
 
   void _onAppDoc(DocumentSnapshot<Map<String, dynamic>> snapshot) async {
-    _targetVersion = Version.parse(snapshot.data()!['version']);
-    if (_targetVersion!.major > _currentVersion!.major) {
-      setState(() {
-        _isBlocked = true;
-      });
-      return;
-    }
+    _minVersion = Version.parse(snapshot.data()!['minVersion']);
+
+    if (_currentVersion! >= _minVersion!) return;
+
+    setState(() {
+      _isBlocked = true;
+    });
   }
 
   @override
@@ -55,7 +57,7 @@ class _VersionGuardState extends State<VersionGuard> {
         theme: AppTheme.build(),
         home: _BlockedScreen(
           currentVersion: _currentVersion!,
-          targetVersion: _targetVersion!,
+          targetVersion: _minVersion!,
         ),
       );
     }
@@ -77,7 +79,12 @@ class _BlockedScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       child: Center(
-        child: Text('Cretino! Aggiorna l\'app!\n$currentVersion -> $targetVersion'),
+        child: Text(
+          'Cretino! Aggiorna l\'app!\n'
+          'Current: $currentVersion\n'
+          'MinVersion: $targetVersion',
+          textAlign: TextAlign.center,
+        ),
       ),
     );
   }
